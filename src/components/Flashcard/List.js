ff705096c0e4f41a289bdcd36967fd565555626d
Guido de Ram <guido.deram@gmail.com>
2019-12-20 21:47:28 -0600

    Added confirmation modal for card deletion

--- src/components/Flashcard/List.js
+++ src/components/Flashcard/List.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table, Button, Container, Row, Col } from 'reactstrap';
+import { Table, Button, Container, Row, Col, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import Tags from './Tags';
 import {db} from "../../firebase.js";
 import {
@@ -12,8 +12,9 @@ export default class List extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            currentCardIndex: 0,
-            cards: []
+            cards: [],
+            modal: false,
+            currentCard: null
         };
     }
 
@@ -25,16 +26,29 @@ export default class List extends React.Component {
           });
     }
 
-    handleDelete = (e) => {
-        e.preventDefault();
+    toggle = () => this.setState({modal: !this.state.modal});
 
-        const cardId = e.target.dataset.card;
+    removeCard = (id) => {
+        const cards = this.state.cards.filter(card => card.id !== id);
+        this.setState({cards});
+    }
+
+    handleDeleteConfirmationClick = () => {
+        const cardId = this.state.currentCard;
         db.collection("cards").doc(cardId).delete().then(() => {
-            console.log("Document successfully deleted!");
-            this.getCards();
+            this.removeCard(cardId);
+            this.toggle();
         }).catch(function(error) {
             console.error("Error removing document: ", error);
         });
+    }
+
+    handleDeleteClick = (e) => {
+        const cardId = e.target.dataset.card;
+
+        this.setState({currentCard: cardId});
+
+        this.toggle();
     }
 
     getCards = (user) => {
@@ -49,51 +63,64 @@ export default class List extends React.Component {
                 });
 
                 if (data && data.length > 0) {
-                    const cards = data.map((card) =>
-                        <tr key={card.id}>
-                            <td>{truncate(card.question, 70, {stripTags: true})}</td>
-                            <td>{truncate(card.answer, 70, {stripTags: true})}</td>
-                            <td>{truncate(card.readmore, 15)}</td>
-                            <td>{card.isPublic ? 'X' : ''}</td>
-                            <td data-card={card.id} onClick={this.handleDelete}>Delete</td>
-                            <td>
-                                <Link to={`/edit/${card.id}`}>Edit</Link>
-                            </td>
-                        </tr>
-                    );
                     this.setState(state => ({
-                        cards: cards
+                        cards: data
                     }));
                 }
             });
     }
 
     render() {
+        const cards = this.state.cards.map((card) =>
+            <tr key={card.id}>
+                <td>{truncate(card.question, 70, {stripTags: true})}</td>
+                <td>{truncate(card.answer, 70, {stripTags: true})}</td>
+                <td>{truncate(card.readmore, 15)}</td>
+                <td>{card.isPublic ? 'X' : ''}</td>
+                <td>
+                    <Link to={`/edit/${card.id}`}><Button color="secondary">Edit</Button></Link>
+                </td>
+                <td>
+                    <Button color="danger" data-card={card.id} onClick={this.handleDeleteClick}>Delete</Button>
+                </td>
+            </tr>
+        );
+
         return (
             <Container>
                 <Row>
                     <Col>
-                    <h1>My cards</h1>
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>Question</th>
-                                <th>Answer</th>
-                                <th>Read more</th>
-                                <th>Public</th>
-                                <th>Delete</th>
-                                <th>Edit</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {this.state.cards}
-                        </tbody>
-                    </Table>
-                    <div>
-                        <Button color="primary" tag={Link} to="/create">Create new card</Button>
-                    </div>
+                        <h1>My cards</h1>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th>Question</th>
+                                    <th>Answer</th>
+                                    <th>Read more</th>
+                                    <th>Public</th>
+                                    <th>Edit</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {cards}
+                            </tbody>
+                        </Table>
+                        <div>
+                            <Button color="primary" tag={Link} to="/create">Create new card</Button>
+                        </div>
                     </Col>
                 </Row>
+                <Modal isOpen={this.state.modal} toggle={this.toggle} backdrop={false}>
+                    <ModalHeader toggle={this.toggle}>Confirm deletion</ModalHeader>
+                    <ModalBody>
+                        Are you sure want to delete this card? This action is irreversible.
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button color="danger" onClick={this.handleDeleteConfirmationClick}>Delete card</Button>{' '}
+                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+                    </ModalFooter>
+                </Modal>
             </Container>
         );
     }
